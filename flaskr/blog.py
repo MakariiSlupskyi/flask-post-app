0038eea7ce274eeb36b0327bad3706cf58333238
MakariiSlupskyi <makariislupskyi@gmail.com>
2024-06-18 00:14:28 +0300

    improve post blueprint

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -81,8 +81,8 @@ def update(id):
 		else:
 			db = get_db()
 			db.execute(
-				''
-				'',
+				'UPDATE post SET title = ?, body = ?'
+				'  WHERE id = ?',
 				(title, body, id)
 			)
 			db.commit()
@@ -91,7 +91,14 @@ def update(id):
 	return render_template('blog/update.html', post=post)
 
 
-
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+	get_post(id)
+	db = get_db()
+	db.execute('DELETE FROM post WHERE id = ?', (id,))
+	db.commit()
+	return redirect(url_for('blog.index'))
 
 
 
